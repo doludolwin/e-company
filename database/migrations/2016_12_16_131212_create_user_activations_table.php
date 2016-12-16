5e92de15658f9f2cc460ad69f0d6fbeb0e14b7d8
dolwin davis <dolwin davis>
2016-12-16 18:49:25 +0530

    migrated user table and activation table,sign out working properly

--- database/migrations/2016_12_16_131212_create_user_activations_table.php
+++ database/migrations/2016_12_16_131212_create_user_activations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUserActivationsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user_activations', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->boolean('admin')->default(false);
-            $table->boolean('activated')->default(false);
-            $table->rememberToken();
+            $table->integer('user_id')->unsigned();
+            $table->string('token')->index();
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_activations');
     }
 }
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -12,10 +12,17 @@
                   @if (Auth::check())
                   <li class="nav-item">
                       <span>
-                          <a class="nav-link" href="{{ url('/home') }}">
-                              <i class="fa fa-sign-out white" aria-hidden="true"></i>
-                              Sign out
+                      <!-- sign out make:auth -->
+                          <a class="nav-link" href="{{ url('/logout') }}"  onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit(); ">
+                              <i class="fa fa-sign-out white" aria-hidden="true">
+                                Sign out  
+                              </i>                                                        
                           </a>
+                      <!-- sign out make:aurh -->
+                          <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                          </form>
                       </span>
                   </li>
                   @else

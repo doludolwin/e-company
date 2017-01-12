578841deeeea01bb2b9ca29c5a03a4d055820582
Sam <thomas.sam64@gmail.com>
2017-01-12 13:22:50 +0530

    sudo

--- resources/views/layouts/wrapper.blade.php
+++ resources/views/layouts/wrapper.blade.php
@@ -17,14 +17,14 @@
             @include("partials.navbar")
             @yield("content")
             @include('partials.footer')
+            
         </div>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.js" charset="utf-8"></script>
         <script src="js/app.js" charset="utf-8"></script>
 
 <!-- Jquery and Bootsrtap 4 cdn -->
         <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.js" charset="utf-8"></script> -->
-        <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" charset="utf-8"></script> -->
-        
+        <!--<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/js/bootstrap.min.js" charset="utf-8"></script> -->    
 <!-- Jquery UI cdn -->
         
 

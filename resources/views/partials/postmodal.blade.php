3606259bb119387165dafaa4483dcefb1bfba1f1
doludolwin <dolwin.davis@gmail.com>
2017-01-13 12:41:59 +0530

    avoid conflicts

--- resources/views/partials/postmodal.blade.php
+++ resources/views/partials/postmodal.blade.php
@@ -8,7 +8,6 @@
           <span aria-hidden="true">&times;</span>
         </button>
         <div class="pro-pic">
-<<<<<<< HEAD
             <img src="/uploads/avatars/{{ $admin->avatar }}" alt="{{ $admin->avatar }}" height="35px" width="35px">
         </div>
         <h6>{{ $user->name }}</h6>
@@ -16,10 +15,7 @@
       <div class="modal-body">
         <form class="" method="post" action="/admin/blogpost" enctype="multipart/form-data" >
         {{ csrf_field() }}
-       
-=======
-
->>>>>>> post-and-card
+        post-and-card
             <div class="post-form">
               <input type="text" name="title" value="" placeholder="Title">
               <input type="hidden" name="published_at" value="{{ Carbon\Carbon::now() }}">
